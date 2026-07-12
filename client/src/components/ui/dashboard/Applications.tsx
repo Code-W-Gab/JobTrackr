8c13520935292b9155d6d130214fb55b8bbed21e
Code-W-Gab <gabconcepcion02@gmail.com>
2026-07-12 12:10:13 +0800

    Implement the dark mode at application page

--- client/src/components/ui/dashboard/Applications.tsx
+++ client/src/components/ui/dashboard/Applications.tsx
@@ -49,14 +49,14 @@ export default function Applications(){
   }, [applications, searchQuery, filter]);
 
   return(
-    <main className="h-full overflow-y-auto p-6 bg-[#f5f7f7] border-l border-indigo-100 ">
+    <main className="h-full overflow-y-auto p-6 bg-[#f5f7f7] border-l border-indigo-100 dark:bg-[#161f2e] dark:border-gray-700">
       <div>
         <div className={`${isOpenApplication ? "hidden" : "block"} shrink-0`}>
           <div className="space-y-1">
-            <h1 className="font-bold text-xl text-gray-800">Applications</h1>
-            <p className="text-xs text-gray-500">{statusValue.totalApplications} application found</p>
+            <h1 className="font-bold text-xl text-gray-800 dark:text-white">Applications</h1>
+            <p className="text-xs text-gray-500 dark:text-gray-400">{statusValue.totalApplications} application found</p>
           </div>
-          <div className="bg-white p-2.5 rounded-lg mt-8 border border-gray-200">
+          <div className="bg-white p-2.5 rounded-lg mt-8 border border-gray-200 dark:bg-[#1E293B] dark:border-gray-700">
             <div className="flex items-center gap-3">
               <div className="w-full">
                 <div className="relative flex items-center">
@@ -68,12 +68,12 @@ export default function Applications(){
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
                     placeholder="Search company, position..."
-                    className="w-full py-1.5 pl-10 pr-4 text-sm text-gray-700 bg-gray-50 border border-gray-200 rounded-xl focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none transition-all placeholder:text-gray-400"
+                    className="w-full py-1.5 pl-10 pr-4 text-sm text-gray-700 bg-gray-50 border border-gray-200 rounded-xl focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none transition-all placeholder:text-gray-400 dark:bg-[#161f2e] dark:border-gray-700"
                   />    
                 </div>
               </div>
 
-              <button onClick={() => setFilterActive(!filterActive)} className="flex items-center gap-2 border border-gray-200 px-5 py-1.5 rounded-xl text-gray-600">
+              <button onClick={() => setFilterActive(!filterActive)} className="flex items-center gap-2 border border-gray-200 px-5 py-1.5 rounded-xl text-gray-600 dark:border-gray-700 dark:text-gray-300">
                 <Funnel size={14}/>
                 <span className="text-sm">Filters</span>
                 {filterActive ? <ChevronUp size={14} /> : <ChevronDown size={14} />}
@@ -85,7 +85,7 @@ export default function Applications(){
               <select 
                 value={filter.platform} 
                 onChange={(e) => setFilter({ ... filter, platform: e.target.value })}
-                className="mt-1.5 w-full border border-gray-200 bg-gray-50 rounded-xl py-1.5 px-3 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="mt-1.5 w-full border border-gray-200 bg-gray-50 rounded-xl py-1.5 px-3 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-[#161f2e] dark:border-gray-700 dark:text-gray-400"
               >
                 {platforms.map((platform) => (
                   <option key={platform} value={platform}>
@@ -97,7 +97,7 @@ export default function Applications(){
               <select 
                 value={filter.locationType}
                 onChange={(e) => setFilter({ ...filter, locationType: e.target.value})}
-                className="mt-1.5 w-full border border-gray-200 bg-gray-50 rounded-xl py-1.5 px-3 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="mt-1.5 w-full border border-gray-200 bg-gray-50 rounded-xl py-1.5 px-3 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-[#161f2e] dark:border-gray-700 dark:text-gray-400"
               >
                 {locationType.map((location) => (
                   <option key={location} value={location}>
@@ -109,7 +109,7 @@ export default function Applications(){
               <select 
                 value={filter.jobType}
                 onChange={(e) => setFilter({ ...filter, jobType: e.target.value })}
-                className="mt-1.5 w-full border border-gray-200 bg-gray-50 rounded-xl py-1.5 px-3 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="mt-1.5 w-full border border-gray-200 bg-gray-50 rounded-xl py-1.5 px-3 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-[#161f2e] dark:border-gray-700 dark:text-gray-400"
               >
                 {jobType.map((job) => (
                   <option key={job} value={job}>
@@ -120,8 +120,8 @@ export default function Applications(){
             </div>
           </div>
 
-          <table className="overflow-x-auto w-full text-left text-sm text-gray-500 mt-4 rounded-lg border border-gray-200 flex-1">
-            <thead className="text-[11px] text-gray-500 uppercase bg-gray-100 ">
+          <table className="overflow-x-auto w-full text-left text-sm text-gray-500 mt-4 border border-gray-200 flex-1 dark:border-gray-700">
+            <thead className="text-[11px] text-gray-500 uppercase bg-gray-100 dark:bg-[#161f2e] dark:text-gray-400">
               <tr >
                 <th scope="col" className="px-5 py-3">COMPANY</th>
                 <th scope="col" className="px-5 py-3">PLATFORM</th>
@@ -137,20 +137,20 @@ export default function Applications(){
             <tbody>
               {filteredApplications.map((application, index) => {
                 return(
-                  <tr key={application._id} className="bg-white border-b border-gray-200 text-xs">
-                    <td className="px-5 py-2 text-sm font-medium text-gray-900 whitespace-nowrap">
+                  <tr key={application._id} className="bg-white border-b border-gray-200 text-xs dark:bg-[#1E293B] dark:border-gray-700">
+                    <td className="px-5 py-2 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                       <div className="flex items-center gap-2">
                         <div className={`text-white text-[13px] font-semibold bg-indigo-600 px-2 py-1 rounded-lg ${getAvatarColor(application.companyName, index)}`}>{getInitials(application.companyName)}</div>
                         <span>{application.companyName}</span>
                       </div>
                     </td>
-                    <td className="px-5 py-2">{application.platform}</td>
-                    <td className="px-5 py-4">{application.jobTitle}</td>
-                    <td className="px-5 py-4">{application.jobType}</td>
-                    <td className="px-5 py-4">{application.locationType}</td>
-                    <td className="px-5 py-4">{formatDate(application.dateApplied)}</td>
-                    <td className="px-5 py-4">{application.salary}</td>
-                    <td className="px-5 py-4">
+                    <td className="px-5 py-2 dark:text-gray-400">{application.platform}</td>
+                    <td className="px-5 py-4 dark:text-gray-400">{application.jobTitle}</td>
+                    <td className="px-5 py-4 dark:text-gray-400">{application.jobType}</td>
+                    <td className="px-5 py-4 dark:text-gray-400">{application.locationType}</td>
+                    <td className="px-5 py-4 dark:text-gray-400">{formatDate(application.dateApplied)}</td>
+                    <td className="px-5 py-4 dark:text-gray-400">{application.salary}</td>
+                    <td className="px-5 py-4 dark:text-gray-400">
                     <div>
                       <Status status={application.status} className="flex w-fit items-center gap-2 rounded-xl px-2.5 py-1"/>
                     </div>
@@ -163,7 +163,7 @@ export default function Applications(){
                             setIsOpenApplication(true)
                             setIndex(index)
                           }}
-                          className="hover:bg-gray-100 text-gray-500 p-1.5 rounded-md">
+                          className="hover:bg-gray-100 text-gray-500 p-1.5 rounded-md dark:text-gray-400 dark:hover:bg-gray-700">
                             <Eye size={13}/>
                         </button>
                         <button
@@ -171,10 +171,10 @@ export default function Applications(){
                             setSelectedId(application._id)
                             setIsModalOpen(true)
                           }} 
-                          className="hover:bg-gray-100 text-gray-500 p-1.5 rounded-md">
+                          className="hover:bg-gray-100 text-gray-500 p-1.5 rounded-md dark:text-gray-400 dark:hover:bg-gray-700">
                             <Pencil size={13}/>
                         </button>
-                        <Link to={application.jobURL} className="hover:bg-gray-100 text-gray-500 p-1.5 rounded-md">
+                        <Link to={application.jobURL} className="hover:bg-gray-100 text-gray-500 p-1.5 rounded-md dark:text-gray-400 dark:hover:bg-gray-700">
                           <ExternalLink size={13}/>
                         </Link>
                         <button
@@ -182,7 +182,7 @@ export default function Applications(){
                             setSelectedId(application._id)
                             setIsDeleteModalOpen(true)
                           }}
-                          className="hover:bg-gray-100 hover:text-red-600 text-gray-500 p-1.5 rounded-md">
+                          className="hover:bg-gray-100 hover:text-red-600 text-gray-500 p-1.5 rounded-md dark:text-gray-400 dark:hover:bg-red-900 dark:hover:text-red-400">
                             <Trash2 size={13}/>
                         </button>
                       </div>
